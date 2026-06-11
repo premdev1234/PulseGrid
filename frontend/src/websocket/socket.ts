62661d13a14dbf6375aabb08d8142aabca44234b
PremDev <120601701+premdev1234@users.noreply.github.com>
2026-06-11 22:01:34 +0530

    Update socket.ts

--- frontend/src/websocket/socket.ts
+++ frontend/src/websocket/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client"
+import { API_BASE_URL } from "../config"
 
-const socket = io("http://localhost:4000")
+const socket = io(API_BASE_URL)
 
 export default socket
